400a0bc186685e668c24c1db3b960be0dfbc5d8b
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2023-02-16 17:22:37 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is a a program that print out a puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
+

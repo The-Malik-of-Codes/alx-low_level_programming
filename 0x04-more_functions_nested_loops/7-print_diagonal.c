9d099d644131343e7676e76edfe0af0d8dc41ff9
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2023-02-24 06:28:39 -0800

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_diagonal - function that draws a diagonal line on the terminal.
  * @n: input value to check
@@ -10,8 +11,10 @@ void print_diagonal(int n)
 int i = 0;
 int j = 0;
 
+
 while (i < n)
-}
+
+{
 while (j <= i)
 {
 if (j != i)

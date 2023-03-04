6f957434f4dc17260d59ec3fb2b055b7fa7bdb09
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2023-03-03 18:57:00 -0800

    Adv Task 1

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,32 +1,34 @@
 #include "main.h"
-
 /**
- * rot13 - function that encodes a string using rot13.
- * @s: pointer to char
- * Return: char
+ *rot13 - encodes strings using rot13.
+ *@s: pointer to string.
+ *
+ *Return: pointer to encoded string.
  */
-
 char *rot13(char *s)
 {
-int i = 0;
-int j = 0;
-char *l = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-char *r13 = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+int stringCount, rotation;
+char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+'Z'};
+char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+'m', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+'M'};
 
-while (*(s + i) != '\0')
+for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
 {
-while (*(l + j) != '\0')
+for (rotation = 0; rotation < 53; rotation++)
 {
-if (*(s + i) == *(l + j))
+if (r1[rotation] == s[stringCount])
 {
+s[stringCount] = r2[rotation];
 break;
-
 }
-j++;
 }
-j = 0;
-
-i++;
 }
 return (s);
 }

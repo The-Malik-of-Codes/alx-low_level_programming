efbdf47ebfb52dec8a4e5dea85e8d0c99b8ae911
The Malik of Codes <ahmadmalaysia2000@gmail.com>
2023-03-23 13:27:27 +0100

    Tasks

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,23 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
-* _memset - function that fills memory with a constant byte
-*@s: first value
-*@b: second value
-*@n: thirth value
-*
-* Return: char with result of memset
-*/
+ * _memset - function that fills memory with a constant byte.
+ *
+ * @s: memory area to return
+ * @b: constant byte
+ * @n: size of bytes
+ * Return: char
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+	unsigned int i = 0;
 
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-return (s);
+	while (i < n)
+	{
+
+		*(s + i) = b;
+		i += 1;
+	}
+	return (s);
 }
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,10 +1,5 @@
+0x07. C - Even more pointers, arrays and strings
 
-
-#	0x07. C - Even more pointers, arrays and strings Tasks and Projects
-
-General overview of what this task and project is all about
-
-*   What are pointers to pointers and how to use them
-*   What are multidimensional arrays and how to use them
-*   What are the most common C standard library functions to manipulate strings
-
+* What are pointers to pointers and how to use them
+* What are multidimensional arrays and how to use them
+* What are the most common C standard library functions to manipulate strings
